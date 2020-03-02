fd1550250c5a9109bb65771e651f37f429b27065
Vitali Karpuk <karpuk-job@yandex.ru>
2020-03-02 19:28:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let b = []
+    let c = []
+    let res = []
+    let res2 = []
+    let end = []
+    for(let i = 0 ; i < expr.length; i = i+2){
+        b.push(expr.slice(i, i+2))
+    }
+    for(let i = 0; i < b.length; i++){
+        if(b[i] === '00'){
+            c.push('')
+        }if(b[i] === '11'){
+            c.push('-')
+        }if(b[i] === '10'){
+            c.push('.')
+        }if(b[i] === '**'){
+            c.push(' ')
+        }
+    }
+    for(let i = 0; i<c.length; i= i+5){
+        res.push(c.slice(i, i+5))
+    }
+    res.forEach(item => {
+        res2.push(item.join(''))
+    })
+    res2.forEach(item => {
+        for (const key in MORSE_TABLE) {
+            if(item === key){
+                end.push(MORSE_TABLE[key])
+            }if(item === '     '){
+                end.push(item = ' ')
+            }
+        }
+    })  
+    return end.join('')
 }
 
 module.exports = {
